6c741405bef3c8d452cb16cdbfd8364f152f068d
berk <sbgonenc@gmail.com>
2024-04-07 13:10:33 +0200

    download functions structured under class

--- download_data.py
+++ download_data.py
@@ -1,22 +1,58 @@
 import os
-
 from settings import OPENTARGETS_LINK, DATA_VERSION, DATA_TYPE
 from utils import call_subprocess
+import tempfile
 
-def download_OT_dataset(data_name, out_dir):
+
+class DownloadPrepareInitialData:
     """
-    Downloads the OpenTargets dataset from the link specified in settings.py
+    Downloads the OpenTargets dataset and joins the json files
     """
 
-    os.chdir(out_dir)
+    def __init__(self, mechanism_of_action_output=None, targets_output=None, molecules_output=None,  temp_dir=None):
 
-    download_url = f"{OPENTARGETS_LINK}/{DATA_VERSION}/output/etl/{DATA_TYPE}/{data_name}"
+        self.temp_dir = temp_dir if temp_dir is not None else tempfile.mkdtemp(prefix="download_OT")
 
-    download_params = ["--recursive", "--no-parent", "--no-host-directories", "--cut-dirs", "8", download_url]
+        print(self.temp_dir)
 
-    return_code, _ = call_subprocess("wget", download_params)
+        self.moa_combined = mechanism_of_action_output if mechanism_of_action_output is not None else os.path.join(self.temp_dir, "moa_combined.json")
+        self.targets_combined = targets_output if targets_output is not None else os.path.join(self.temp_dir, "targets_combined.json")
+        self.molecules_combined = molecules_output if molecules_output is not None else os.path.join(self.temp_dir, "molecules_combined.json")
 
-    if return_code != 0:
-        raise Exception("Download failed")
+    def process(self):
+        self.download_data("targets")
+        self.join_json_files(os.path.join(self.temp_dir, "targets"), self.targets_combined)
 
-    return f"{DATA_TYPE}_{DATA_VERSION}.json"
+        self.download_data("mechanismOfAction")
+        self.join_json_files(os.path.join(self.temp_dir, "mechanismOfAction"), self.moa_combined)
+
+        self.download_data("molecule")
+        self.join_json_files(os.path.join(self.temp_dir, "molecule"), self.molecules_combined)
+
+
+    def download_data(self, data_name):
+        """
+        Downloads the OpenTargets dataset from the link specified in settings.py
+        """
+
+        download_url = f"{OPENTARGETS_LINK}/{DATA_VERSION}/output/etl/{DATA_TYPE}/{data_name}"
+
+        download_params = ["--recursive", "--no-parent", "--no-host-directories", "--cut-dirs", "8", "--directory-prefix", self.temp_dir,  download_url]
+
+        return_code, _ = call_subprocess("wget", download_params)
+
+        if return_code != 0:
+            raise Exception("Download failed")
+
+        print(f"Downloaded {data_name}")
+        return 1
+
+    def join_json_files(self, input_folder, output_file):
+        """
+        Joins all the json lines files in the input_folder into one file
+        """
+        return_code, _ = call_subprocess("cat", [f"{input_folder}/*.json"], outfile=output_file)
+
+        if return_code != 0:
+            raise Exception("Joining failed")
+        return 1
